6b55a088db7e9c807f260e33d76fbe66deaddcd5
Haorui Zhou <haorui2002@gmail.com>
2025-03-08 15:19:35 -0500

    Update layout.tsx

--- my-next-app/app/layout.tsx
+++ my-next-app/app/layout.tsx
@@ -3,7 +3,7 @@
 import "./globals.css"; // Ensure your CSS file is imported
 import Link from "next/link";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <body>
