38f29309d3b813b2ca94ed8ab32b502437f86fa9
Ella Harding <ehardin8@uwo.ca>
2025-03-12 00:00:05 -0400

    small changes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -35,14 +35,8 @@ function App() {
             <ul className="nav-links">
               <li><Link to="/">Home</Link></li>
               <li><Link to="/fsae-simulator">FSAE Simulator</Link></li>
-              <li><Link to="/fsae-simulator">Aero</Link></li>
-              <li><Link to="/fsae-simulator">Brakes/Pedals</Link></li>
-              {/*<li><Link to="/fsae-simulator">Composites</Link></li>*/}
-              {/*<li><Link to="/fsae-simulator">Ergo</Link></li>*/}
-              <li><Link to="/GLV">GLV</Link></li>
-              <li><Link to="/oldGLV">oldGLV</Link></li>
-              <li><Link to="/fsae-simulator">Suspension</Link></li>
-              <li><Link to="/fsae-simulator">Wheels/Tires</Link></li>
+              {/* Protected routes <li><Link to="/GLV">GLV</Link></li>
+              <li><Link to="/oldGLV">oldGLV</Link></li>*/}
               <li><Link to="/WFRDownloader">WFR Downloader</Link></li>
               <li><Link to="/login">Login</Link></li>
               <li><LogoutButton /></li>
